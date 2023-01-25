4b5c022cc9dcbe459f8cf297410babd988a63e25
Phoenix <90419301+lancome1@users.noreply.github.com>
2023-01-24 21:30:49 -0500

    Create 578GetHighestAnswerRateQuestion.sql

---
+++ SQLLeetCode/578GetHighestAnswerRateQuestion.sql
@@ -0,0 +1,12 @@
+# Note: answer rate = answer actions / total non-answer actions (e.g., show and skip)
+SELECT TOP 1 question_id AS survey_log
+FROM survey_log
+GROUP BY question_id
+ORDER BY COUNT(answer_id)/(COUNT(*)-COUNT(answer_id)) DESC;
+
+# Another variant
+SELECT question_id AS 'survey_log'
+FROM survey_log
+GROUP BY question_id
+ORDER BY COUNT(answer_id) / SUM(IF(action = 'show', 1, 0)) DESC
+LIMIT 1;
