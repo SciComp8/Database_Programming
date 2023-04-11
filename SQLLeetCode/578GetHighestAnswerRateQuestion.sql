c350b4a1ae5f3548805ef98fe107a1ec7232e2e9
Anni <90419301+anniliu9@users.noreply.github.com>
2023-04-10 21:40:29 -0400

    Update 578GetHighestAnswerRateQuestion.sql

--- SQLLeetCode/578GetHighestAnswerRateQuestion.sql
+++ SQLLeetCode/578GetHighestAnswerRateQuestion.sql
@@ -2,7 +2,7 @@
 SELECT TOP 1 question_id AS survey_log
 FROM survey_log
 GROUP BY question_id
-ORDER BY COUNT(answer_id)/(COUNT(*)-COUNT(answer_id)) DESC;
+ORDER BY COUNT(answer_id)/(COUNT(*)-COUNT(answer_id)) DESC; 
 
 # Another variant
 SELECT question_id AS 'survey_log'
