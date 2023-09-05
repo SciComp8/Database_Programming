c15edbd1e994fe30fd9b2e664e3e5ba6f1ebf543
Anni <90419301+anniliu7@users.noreply.github.com>
2023-09-05 18:46:42 -0400

    Create 1082SalesAnalysisI.sql

---
+++ SQLLeetCode/1082SalesAnalysisI.sql
@@ -0,0 +1,9 @@
+select distinct seller_id
+from Sales
+group by seller_id
+having sum(price) = (
+    select sum(price) as max_price
+    from Sales
+    group by seller_id
+    order by max_price desc
+    limit 1)
