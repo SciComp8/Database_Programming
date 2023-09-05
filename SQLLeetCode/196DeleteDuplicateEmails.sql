d6a540c849a5cd22027167a19199fb030be94782
Anni <90419301+anniliu7@users.noreply.github.com>
2023-09-05 19:32:20 -0400

    Create 196DeleteDuplicateEmails.sql

---
+++ SQLLeetCode/196DeleteDuplicateEmails.sql
@@ -0,0 +1,10 @@
+# Approach 1:
+DELETE p 
+FROM Person p, Person q 
+WHERE p.id > q.id AND p.email = q.email 
+
+  # Approach 2:
+DELETE p
+FROM person p
+JOIN person q
+    ON p.email = q.email AND p.id > q.id;  
