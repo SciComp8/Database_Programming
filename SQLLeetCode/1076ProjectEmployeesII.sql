16e277ef91ade0034f0cc33d40ca61d25e875c47
Anni <90419301+anniliu7@users.noreply.github.com>
2023-09-04 20:07:25 -0400

    Create 1076ProjectEmployeesII.sql

---
+++ SQLLeetCode/1076ProjectEmployeesII.sql
@@ -0,0 +1,16 @@
+# Approach 1
+SELECT p.project_id
+FROM Project AS p
+GROUP BY p.project_id
+HAVING COUNT(p.employee_id) = 
+    (SELECT max(p1.employee_num)
+    FROM (SELECT project_id, COUNT(employee_id) AS employee_num
+         FROM Project
+         GROUP BY project_id) AS p1
+    );
+
+# Approach 1
+SELECT TOP 1 WITH TIES project_id
+FROM Project
+GROUP BY project_id
+ORDER BY COUNT(employee_id) DESC;
