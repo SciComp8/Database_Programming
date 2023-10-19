9fe8c114b8b1a45d04f8114967a2d949034018d9
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-19 19:35:06 -0400

    Update Select_Data.sql

--- Utilities/Select_Data.sql
+++ Utilities/Select_Data.sql
@@ -16,15 +16,23 @@ SELECT var1, var2
 FROM table1
 WHERE var1='value1' AND var2='value2';
 
-
+SELECT SUM(var2)
+FROM table1;
+  
+#Do the summation of var2 per categorical level in var1
 SELECT var1, SUM(var2)
 FROM table1
-GROUP BY var1
+GROUP BY var1;
 
-SELECT var1, SUM(var2)
+SELECT var1, SUM(var2) AS sum_var2 #Add an alias
+FROM table1
+GROUP BY var1;
+
+#HAVING filters the results of an aggregated function
+SELECT var1, SUM(var2) AS sum_var2
 FROM table1
 GROUP BY var1
-HAVING SUM(var2) <= 200;
+HAVING SUM(var2) <= 200; 
 
 SELECT var1, var2
 FROM table1
