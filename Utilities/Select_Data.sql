53150560403788eba03d28afed94604f4f53d33b
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-19 07:57:32 -0400

    Update Select_Data.sql

--- Utilities/Select_Data.sql
+++ Utilities/Select_Data.sql
@@ -4,6 +4,7 @@
 #GROUP
 #HAVING
 #ORDER BY
+#Aggregate function: COUNT()/SUM()/AVG()/MIN()/MAX()
 
 SELECT var1, var2
 FROM table1;
@@ -14,3 +15,22 @@ FROM table1;
 SELECT var1, var2
 FROM table1
 WHERE var1='value1' AND var2='value2';
+
+
+SELECT var1, SUM(var2)
+FROM table1
+GROUP BY var1
+
+SELECT var1, SUM(var2)
+FROM table1
+GROUP BY var1
+HAVING SUM(var2) <= 200;
+
+SELECT var1, var2
+FROM table1
+ORDER BY var2;
+
+SELECT var1, var2
+FROM table1
+ORDER BY var2 DESC;
+
