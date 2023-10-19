8e5d1eff5a9930912448e8b0fda6383b96528e11
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-19 19:52:30 -0400

    Update Select_Data.sql

--- Utilities/Select_Data.sql
+++ Utilities/Select_Data.sql
@@ -5,7 +5,9 @@
 #HAVING
 #ORDER BY
 #Aggregate function: COUNT()/SUM()/AVG()/MIN()/MAX()
-
+#None-aggregate function: ABS()/MOD()/CEILING()/ADDDATE()/ISNULL()
+# More: https://www.techonthenet.com/mysql/functions/index.php
+  
 SELECT var1, var2
 FROM table1;
 
@@ -41,4 +43,18 @@ ORDER BY var2;
 SELECT var1, var2
 FROM table1
 ORDER BY var2 DESC;
+
+#Make values in each cell of the column `music_name` all uppercase
+SELECT UCASE(music_name) AS uppercase_music_name
+FROM table2;
+
+#Take average of 2 columns
+SELECT (var1 + var2) / 2 AS avg_var12
+FROM table2;
+
+#If-else: recode values within a column
+SELECT IFF(var1='Yes', 1, 0) AS var1_01
+FROM table2;
+
+#SELECT ABS(30 * -1);
 
