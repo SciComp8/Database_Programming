bdca6d2fd7ca2b1ffe5fa1ac1ecb9760584a8697
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-19 20:14:12 -0400

    Update Join_Data.sql

--- Utilities/Join_Data.sql
+++ Utilities/Join_Data.sql
@@ -1,1 +1,3 @@
-#
+#When we join the tables, there always needs to be a column that is common to both of the tables
+#Inner joins: return only the records where data exists in the joined field in both tables
+#Left joins: return all of the records from the table on the left along with matching records form the table on the right
