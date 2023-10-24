1462869741200fdc67844a2e1461d2f8f2aefdee
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-24 08:45:35 -0400

    Update Subqueries.sql

--- Utilities/Subqueries.sql
+++ Utilities/Subqueries.sql
@@ -2,19 +2,25 @@
 
 #Subquery in the WHERE clause generates values or lists to be used in the filtering criteria
 #There can only be 1 varibale in the SELECT clause for this type of subquery
-#Example: obtain information on patients who have no records in the follow-up table
+#Example 1: obtain information on patients who have no records in the follow-up table
 SELECT patient_id, treatment
 FROM patients
 WHERE patient_id NOT IN
   (SELECT patient_id
    FROM follow);
 
-#Another solution using the LEFT JOIN
+#Alternative solution using the LEFT JOIN
 SELECT p.patient_id, p.treatment
 FROM patients as p
   LEFT JOIN visits as v
   ON p.patient_id = v.patient_id
 WHERE v.patient_id IS NULL;
+
+#Example 2: find the second highest salary
+SELECT MAX(Salary) AS second_highest_salary
+FROM Employee
+WHERE Salary < (Select MAX(Salary) FROM Employee);
+#WHERE Salary NOT IN (SELECT MAX(Salary) FROM Employee);
 
 #Subquery in the FROM clause generates a derived table to select data from
 #This is useful when we need to select from aggregated data
