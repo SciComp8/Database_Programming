dec4326d89877baf4516eea7929244b8c65ae6ac
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-24 10:30:08 -0400

    Update Union.sql

--- Utilities/Union.sql
+++ Utilities/Union.sql
@@ -2,4 +2,30 @@
 #Prerequisite: we must have identical columns to append
 #Duplicate rows between top set of results and botton set of results are removed
 
-SELECT
+#Syntax
+SELECT var1, var2, var3
+FROM table1
+
+UNION
+
+SELECT var1, var2, var3
+FROM table2;
+
+#Example
+SELECT * 
+FROM company1
+
+UNION
+
+SELECT *
+FROM company2;
+
+#Union all: Duplicate rows between top set of results and botton set of results are NOT removed
+#Example
+SELECT * 
+FROM company1
+
+UNION ALL
+
+SELECT *
+FROM company2;
