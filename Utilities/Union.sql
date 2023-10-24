4b82d0823d9b40792b191dc516008314f3b81d31
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-24 10:27:02 -0400

    Create Union.sql

---
+++ Utilities/Union.sql
@@ -0,0 +1,5 @@
+#Append the results from one query to the results from another query -> connect tables from top to bottom
+#Prerequisite: we must have identical columns to append
+#Duplicate rows between top set of results and botton set of results are removed
+
+SELECT
