30e00e4053e8b66f80c1cd7d1c01122929e9c139
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-25 09:38:31 -0700

    Create Create_Index.sql

---
+++ Utilities/Create_Index.sql
@@ -0,0 +1,5 @@
+CREATE INDEX index_name
+  ON table_name (var_to_be_indexed_1, var_to_be_indexed_2);
+
+CREATE UNIQUE INDEX index_name
+  ON table_name (var_to_be_indexed_1, var_to_be_indexed_2, var_to_be_indexed_3);
