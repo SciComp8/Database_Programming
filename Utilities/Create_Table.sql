23c46e5daecd4844438fdc9e0d4fc56dc66f9ce0
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-25 10:20:23 -0700

    Update Create_Table.sql

--- Utilities/Create_Table.sql
+++ Utilities/Create_Table.sql
@@ -1,8 +1,9 @@
 CREATE TABLE table_name (
-  PRIMARY KEY(key_name_1, key_name_2),
+  PRIMARY KEY(key_name_1, key_name_2), #this is a composite key
   var_1 TEXT(4),
   var_2 BIT,
   var_3 BIT,
   var_4 LONGTEXT,
-  var_5 CHAR(6)
+  var_5 CHAR(6),
+  var_6 TINYINT(3) AUTO_INCREMENT #autonumber
 );
