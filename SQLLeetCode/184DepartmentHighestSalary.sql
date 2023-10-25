236695a9ad8b0180af478eb3f60ade287eea9cf5
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-25 01:04:05 -0700

    Update 184DepartmentHighestSalary.sql

--- SQLLeetCode/184DepartmentHighestSalary.sql
+++ SQLLeetCode/184DepartmentHighestSalary.sql
@@ -11,3 +11,13 @@ JOIN employee AS e
     ON ms.departmentId = e.departmentid AND ms.max_salary = e.salary
 JOIN department AS d
     ON e.departmentId = d.id;
+
+# Alternative solution: left join from a subquery that returns the maximum salary in each department
+SELECT d.Name AS Department, e.Name As Employee, e.Salary AS Salary
+FROM Employee AS e
+LEFT JOIN Department AS d
+ON e.DepartmentId = d.Id
+WHERE (e.DepartmentId, e.Salary) in 
+  (SELECT DepartmentId, max(Salary)
+   FROM Employee
+   GROUP BY DepartmentId);
