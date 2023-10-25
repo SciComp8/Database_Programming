204aab1e96e80e9e97866ff00ba258a181fd7c28
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-25 01:05:56 -0700

    Update 184DepartmentHighestSalary.sql

--- SQLLeetCode/Subquery/Medium/184DepartmentHighestSalary.sql
+++ SQLLeetCode/Subquery/Medium/184DepartmentHighestSalary.sql
@@ -1,1 +1,9 @@
-#
+#Main idea: left join 2 tables from a subquery that returns the maximum salary in each department
+SELECT d.Name AS Department, e.Name As Employee, e.Salary AS Salary
+FROM Employee AS e
+LEFT JOIN Department AS d
+ON e.DepartmentId = d.Id
+WHERE (e.DepartmentId, e.Salary) in 
+  (SELECT DepartmentId, max(Salary)
+   FROM Employee
+   GROUP BY DepartmentId);
