52ad7f2f60dd8fef36121e7d4c6371537f47b077
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-25 10:21:11 -0700

    Update Delete_Index.sql

--- Utilities/Delete_Index.sql
+++ Utilities/Delete_Index.sql
@@ -1,3 +1,4 @@
-#Notice that we will not see anything after run the following query
+#If the table exists
+#Notice that we will not see anything after we run the following query
 DROP INDEX index_name
   ON table_name;
