722d5e03fa0f2b4cebfd630b9c1ca2f02fca6d44
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-25 10:24:34 -0700

    Create Alter_PK_MySQL.sql

---
+++ Utilities/Alter_PK_MySQL.sql
@@ -0,0 +1,7 @@
+#Add a primary key to an existing table
+ALTER TABLE table_name
+  ADD PRIMARY KEY(var_as_pk_1, var_as_pk_2);
+
+#Remove a primary key from an existing table
+ALTER TABLE table_name
+  DROP PRIMARY KEY;
