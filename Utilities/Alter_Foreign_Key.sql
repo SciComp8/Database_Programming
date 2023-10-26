03bdee5c15a3e92bdcfd0eac4fd92762ca2cf0b0
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-26 08:40:07 -0700

    Update and rename Add_Foreign_Key.sql to Alter_Foreign_Key.sql

--- Utilities/Alter_Foreign_Key.sql
+++ Utilities/Alter_Foreign_Key.sql
@@ -1,5 +1,13 @@
+#Add a foreign key into an existing table
 ALTER TABLE child_table_name
   ADD FOREIGN KEY (fk_1) REFERENCES parent_table_name_1(pk_1)
     ON UPDATE CASCADE,
   ADD FOREIGN KEY (fk_2) REFERENCES parent_table_name_2(pk_2)
     ON UPDATE CASCADE;
+
+#Remove a foreign key from an existing table
+ALTER TABLE child_table_name
+  DROP FOREIGN KEY fk_1,
+  DROP FOREIGN KEY fk_2;
+
+
