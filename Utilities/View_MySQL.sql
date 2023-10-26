26632a0c99bbb2c1fde90dba562c9ea89c258df3
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-26 09:07:35 -0700

    Update View_MySQL.sql

--- Utilities/View_MySQL.sql
+++ Utilities/View_MySQL.sql
@@ -5,3 +5,22 @@
 #Views enable us to save intricate queries within the database. Instead of having to run a complicated SQL query every time we wish to retrieve data, we can simply execute a straightforward query like this: SELECT column_list FROM view_name;
 #Views enable you to organize data tailored for a particular set of users.
 #Views play a crucial role in preserving database security. Instead of providing users direct access to the database tables, you establish a view to expose only the essential data, and then grant users access to that view.
+
+#Create a view
+CREATE VIEW view_name AS
+  SELECT_query
+
+#Example:
+CREATE VIEW member_contacts AS
+    SELECT first_name, last_name, email, phone_number, institute_name
+    FROM member AS m
+      INNER JOIN institute AS i
+      ON m.institute_id = i.institute_id
+    ORDER BY first_name;
+
+#Change a view
+CREATE OR REPLACE VIEW view_name AS
+  SELECT_query;
+
+#Delete a view
+DROP VIEW view_name;
