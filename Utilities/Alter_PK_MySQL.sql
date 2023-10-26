c31b5c40a3ddf262f94d0e6d6ad3a5e18bd9a1f1
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-26 08:54:06 -0700

    Update Alter_PK_MySQL.sql

--- Utilities/Alter_PK_MySQL.sql
+++ Utilities/Alter_PK_MySQL.sql
@@ -1,3 +1,8 @@
+#In a relational database, all rows in each table must be unique.
+#A primary key is a unique index that's designated as the identifier for each row of a table.
+#Values entered into the primary key must be (1) unique in each row; (2) non-null
+#If there is more than 1 column as the primary key, its **combination** of values in those columns must be unique.
+
 #Add a primary key to an existing table
 ALTER TABLE table_name
   ADD PRIMARY KEY(var_as_pk_1, var_as_pk_2);
