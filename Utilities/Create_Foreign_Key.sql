fe91b2c5840ce3feea1b14861b03c020313bb4e7
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-26 08:46:28 -0700

    Update Create_Foreign_Key.sql

--- Utilities/Create_Foreign_Key.sql
+++ Utilities/Create_Foreign_Key.sql
@@ -1,11 +1,20 @@
 CREATE TABLE child_table_name(
-  PRIMARY KEY (pk_1, pk_2),
-  var_1 TINYINT(3),
-  var_2 TINYINT(2),
-  FOREIGN KEY (fk_1) REFERENCES parent_table_name_1(pk_1)
+  PRIMARY KEY (c_pk_1, c_pk_2),
+  c_pk_1 TINYINT(3),
+  c_pk_2 TINYINT(2),
+  FOREIGN KEY (c_pk_1) REFERENCES parent_table_name_1(p_pk_1) #c_pk_1 = p_pk_1
     ON UPDATE CASCADE,
-  FOREIGN KEY (fk_2) REFERENCES parent_table_name_2(pk_2)
+  FOREIGN KEY (c_pk_2) REFERENCES parent_table_name_2(p_pk_2) #c_pk_2 = p_pk_2
     ON UPDATE CASCADE
 );
 
 #TD: ON UPDATE CASCADE: sync the parent table and child table
+
+#Assign a name to a FOREIGN KEY 
+CREATE TABLE genomic_project (
+    sample_id INT AUTO_INCREMENT PRIMARY KEY,
+    patient_id INT,
+    sequencing_tech VARCHAR(100),
+    CONSTRAINT fk_genomic FOREIGN KEY (patient_id) #name the fk with fk_genomic
+        REFERENCES whole_project (patient_id)
+);
