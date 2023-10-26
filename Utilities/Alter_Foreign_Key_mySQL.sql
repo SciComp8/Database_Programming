44253c9869ecbec0cbd9148618f56973b0e45387
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-26 08:49:27 -0700

    Update and rename Alter_Foreign_Key.sql to Alter_Foreign_Key_mySQL.sql

--- Utilities/Alter_Foreign_Key_mySQL.sql
+++ Utilities/Alter_Foreign_Key_mySQL.sql
@@ -5,9 +5,14 @@ ALTER TABLE child_table_name
   ADD FOREIGN KEY (fk_2) REFERENCES parent_table_name_2(pk_2)
     ON UPDATE CASCADE;
 
+#Add a foreign key with the assigned name (fk_name) into an existing table
+ALTER TABLE child_table_name
+ADD CONSTRAINT fk_name FOREIGN KEY (fk_column)
+   REFERENCES parent_table_name(pk_column)
+
 #Remove a foreign key from an existing table
 ALTER TABLE child_table_name
-  DROP FOREIGN KEY fk_1,
-  DROP FOREIGN KEY fk_2;
+  DROP FOREIGN KEY fk_name_1,
+  DROP FOREIGN KEY fk_name_2;
 
 
