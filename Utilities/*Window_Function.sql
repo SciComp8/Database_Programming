773ec36380e6189d4035a3bb81fdfef1d8cd2d25
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-26 21:18:04 -0700

    Update *Window_Function.sql

--- Utilities/*Window_Function.sql
+++ Utilities/*Window_Function.sql
@@ -1,3 +1,10 @@
 #The window function allow us to add aggreate values to non-grouped result sets, 
 #so that we leave the non-aggregated variables the way they are, and add columns that contain aggregated values
 #We can add 'down-column' calculations (e.g., average, max, min, sum) to non-grouped results, based on certain rows of the data
+
+#OVER()
+#Example: add the overall average to each row
+SELECT patient_id, start_date, end_date,
+  DATEDIFF(end_date, start_date) AS duration_days,
+  AVG(DATEDIFF(end_date, start_date)) OVER() AS avg_duration
+FROM date_table;
