79b175e729ed6b5793cab49f905d65f2cf7df1b3
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-26 20:33:59 -0700

    Update *Window_Function.sql

--- Utilities/*Window_Function.sql
+++ Utilities/*Window_Function.sql
@@ -1,3 +1,3 @@
-#Allow us to add aggreate values to non-grouped result sets, 
+#The window function allow us to add aggreate values to non-grouped result sets, 
 #so that we leave the non-aggregated variables the way they are, and add columns that contain aggregated values
 #We can add 'down-column' calculations (e.g., average, max, min, sum) to non-grouped results, based on certain rows of the data
