a64cba26b5aaf07bdd80fcf26d5b16c2c7652513
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-27 05:30:21 -0700

    Update 184DepartmentHighestSalary.sql

--- SQLLeetCode/Subquery/Medium/184DepartmentHighestSalary.sql
+++ SQLLeetCode/Subquery/Medium/184DepartmentHighestSalary.sql
@@ -7,3 +7,20 @@ WHERE (e.DepartmentId, e.Salary) in
   (SELECT DepartmentId, max(Salary)
    FROM Employee
    GROUP BY DepartmentId);
+
+#Alternative solution:
+SELECT *
+FROM (
+	SELECT 
+		d.Name AS Department, 
+		e.Name As Employee, 
+		e.Salary AS Salary,
+		DENSE_RANK() OVER (
+			PARTITION BY e.DepartmentId
+			ORDER BY e.salary DESC) AS salary_rank
+	FROM 
+		Employees AS e
+		LEFT JOIN Department AS d 
+			ON e.DepartmentId = d.Id
+	) AS t
+WHERE salary_rank = 1;
