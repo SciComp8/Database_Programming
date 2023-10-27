25ec8f56bde78a34cfd2203fe04ed1aa5992345c
Anni <90419301+ScienceComputing@users.noreply.github.com>
2023-10-27 06:20:28 -0700

    Update Moving_Average.sql

--- Utilities/Moving_Average.sql
+++ Utilities/Moving_Average.sql
@@ -1,1 +1,4 @@
-#
+#The main idea is to examine how the averages behave over time instead of examining the behavior of the original or raw data points
+#Working with moving averages gives us a better representation of the time series, since longer-term trends are much easier to see with moving averages than with raw data points
+#Smoothing is the main objective of the moving average technique; it is used to remove noise from the data. With less noise, the real trends in time series data are easier to see.
+#Analysis of confirmed COVID-19 cases: moving averages help show how the trend of number of confirmed cases is changing over time.
